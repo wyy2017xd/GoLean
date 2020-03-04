4606ab70d2c1a69f1939a45df32c583b06e2c41a
wangyeyu <wangyeyu@inspur.com>
2020-03-04 21:40:46 +0800

    slice capacity increase

---
+++ mySlice.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := make([]int, 5)
+	b := a[0:5]
+	a = append(a, 1)//创建了新数组,cap*2
+	a[1] = 5//对有影响
+	fmt.Println(b, cap(b))
+	// [0 0 0 0]
+	fmt.Println(a, cap(a))
+	// [0 5 0 0 0 1]</pre>
+
+	s := []int{}
+	for i := 0; i < 16; i++ {
+		s = append(s, i)
+		fmt.Println(cap(s))
+	}
+	/*当需要的容量超过原切片容量的两倍时，会使用需要的容量作为新容量。
+	当原切片长度小于1024时，新切片的容量会直接翻倍。而当原切片的容量大于等于1024时，会反复地增加25%，直到新容量超过所需要的容量*/
+}
